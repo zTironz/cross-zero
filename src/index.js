fabed1a8e1f7bc0f302411af50da1a9c456d14c7
tiron <ole_g92@bk.ru>
2022-12-12 20:14:16 +0300

    game component changes

--- src/index.js
+++ src/index.js
@@ -52,12 +52,12 @@ function Square(props) {
     constructor(props) {
         super(props);
         this.state = {history: [{squares: [null,null,null,null,null,null,null,null,null]}],
-            xIsNext: true}
-
+        stepNumber: 0,
+        xIsNext: true}
     }
 
     handleClick(i) {
-        const history = this.state.history;
+        const history = this.state.history.slice(0, this.state.stepNumber + 1);
         const current = history[history.length - 1];
         const squares = current.squares.slice();
         if (calculateWinner(squares) || squares[i]) {
@@ -68,21 +68,31 @@ function Square(props) {
           history: history.concat([{
             squares: squares,
           }]),
+          stepNumber: history.length,
           xIsNext: !this.state.xIsNext,
         });
       }
 
+    jumpTo(step) {
+        this.setState({
+            stepNumber: step,
+            xIsNext: (step % 2) === 0
+        })
+    }
+
 
     render() {
         const history = this.state.history;
-        const current = history[history.length -1]
+        console.log(this.state, 'state')
+        console.log(history)
+        const current = history[this.state.stepNumber];
         const winner = calculateWinner(current.squares)
         const moves = history.map((step,move) => {
             const desc = move ?
             'move step ' + move :
             'move to the start';
             return (
-                <li>
+                <li key={move}>
                     <button onClick={() => this.jumpTo(move)}>{desc}</button>
                 </li>
             )
